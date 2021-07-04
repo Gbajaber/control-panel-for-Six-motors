1b3e0b6885d85a13ff3696bc2ee0806855b5f5f7
Gbajaber <85759827+Gbajaber@users.noreply.github.com>
2021-07-04 15:48:09 +0300

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 04 يوليو 2021 الساعة 05:26
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `user`
+--
+
+CREATE TABLE `user` (
+  `Motor1` int(11) NOT NULL,
+  `Motor2` int(11) NOT NULL,
+  `Motor3` int(11) NOT NULL,
+  `Motor4` int(11) NOT NULL,
+  `Motor5` int(11) NOT NULL,
+  `Motor6` int(11) NOT NULL,
+  `On` int(11) NOT NULL,
+  `OFF` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
